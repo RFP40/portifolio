129b963793c13c42cbbe0592848b3072fbd0b0c2
Rafael Mesquita Torres <rafamesto@gmail.com>
2026-04-24 08:39:50 -0300

    Adição Projetos

--- script/script.js
+++ script/script.js
@@ -13,3 +13,36 @@ function typeWriter() {
 }
 
 typeWriter();
+
+/* AQUI COMEÇA O CARROSSEL*/
+
+let index = 0;
+
+function atualizar() {
+  const track = document.querySelector(".track");
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function avancar() {
+  const cards = document.querySelectorAll(".card");
+
+  index++;
+
+  if (index >= cards.length) {
+    index = 0; // volta pro primeiro
+  }
+
+  atualizar();
+}
+
+function voltar() {
+  const cards = document.querySelectorAll(".card");
+
+  index--;
+
+  if (index < 0) {
+    index = cards.length - 1; // vai pro último
+  }
+
+  atualizar();
+}
